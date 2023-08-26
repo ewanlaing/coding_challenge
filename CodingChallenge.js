64acc2b67e8905ee4871602fe7bf389f91783506
ewanlaing <117400398+ewanlaing@users.noreply.github.com>
2023-08-26 13:17:01 +0100

    2nd main commit

--- CodingChallenge.js
+++ CodingChallenge.js
@@ -5,17 +5,19 @@
 const countingSumPairs = (x, numArray) => {
     //I then declare the output, initially zero, which will return with the correct value at the end of the function
   let Output = 0;
+  //I then create a copy of the initial array to avoid altering the initial data (in case it needs use later on)
+  let copyArray = [... numArray];
   //We then loop through the array of numbers examining each number individually
-  for(i = 0; i < numArray.length; i++){
+  for(i = 0; i < copyArray.length; i++){
     //For each number we then run a second loop through the numbers, in order to compare the first number with every other number in that array.
-    for(j = 0; j< numArray.length; j++){
+    for(j = 0; j< copyArray.length; j++){
         //When checking the two numbers against each other, we first check if they add together to make the value of x. We also check that they are not the same index position in the list to avoid adding an array entry with itself.
-        if((numArray[i] + numArray[j] === x) && (i !== j)){
+        if((copyArray[i] + copyArray[j] === x) && (i !== j)){
             //If the conditions are met we increase the value of the output.
             Output += 1;
             //Since the pair of numbers has now been used, we convert the numbers to strings that will not be capable of adding to the desired sum.
-            numArray[i] = "fizz";
-            numArray[j] = "buzz";
+            copyArray[i] = "fizz";
+            copyArray[j] = "buzz";
         }
     }
   }
